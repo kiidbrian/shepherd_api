5dbbe2869251c0b590de014dd2e7c520c6dc1124
Brian Paintsil <brnpaintsil@gmail.com>
2019-01-21 19:40:15 +0000

    created the models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class Abstract(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+class User(Abstract):
+    name = models.TextField()
+    phone = models.TextField()
+    email = models.EmailField(max_length=75)
+    role = models.TextField()
+    active = models.BooleanField(default=False)
+    password = models.TextField()
+
+    class Meta:
+        pass
+
+class Member(Abstract):
+    name = models.TextField()
+    phone = models.TextField()
+    email = models.EmailField(max_length=75)
+    address = models.TextField()
+
+    class Meta:
+        pass
+    
+class Attendance(models.Model):
+    date = models.DateTimeField(auto_now_add=True)
+    member_id = models.ForeignKey(Member, related_name="attendance", on_delete=models.CASCADE, null=True)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+
