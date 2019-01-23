d8f6e3e200fd342002b6bc90a46a798d5ba0a2f1
Brian Paintsil <brnpaintsil@gmail.com>
2019-01-23 22:42:43 +0000

    add Member and Attendance ModelSerializers

--- api/serializers.py
+++ api/serializers.py
@@ -23,3 +23,32 @@ class UserSerializer(serializers.ModelSerializer):
         instance.active = validated_data.get('active', instance.active)
         instance.save()
         return instance
+
+
+class MemberSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Member
+        fields =('id', 'name', 'phone', 'email', 'address')
+
+    def create(self, validated_data):
+        """
+        Create and return a new `Member` instance, given the validated data.
+        """
+        return Member.objects.create(**validated_data)
+    
+    def update(self, instance, validated_data):
+        """
+        Update and return an existing `Member` instance, given the validated data.
+        """
+        instance.name = validated_data.get('name', instance.name)
+        instance.phone = validated_data.get('phone', instance.phone)
+        instance.email = validated_data.get('email', instance.email)
+        instance.address = validated_data.get('address', instance.address)
+        instance.save()
+        return instance
+
+
+class AttendanceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Attendance
+        fields= ('id', 'date', 'member_id', 'user_id')
