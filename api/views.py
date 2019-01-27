b7b70f92bd5c39961170036a03d1724256a930d0
Brian Paintsil <brnpaintsil@gmail.com>
2019-01-27 02:09:20 +0000

    add Member views

--- api/views.py
+++ api/views.py
@@ -45,4 +45,46 @@ def user_detail(request,pk):
     if request.method == "DELETE":
         user.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+@api_view(['GET', 'POST'])
+def member_list(request):
+    """
+    List all members, or create a new member.
+    """
+    if request.method == 'GET':
+        members = Member.objects.all()
+        serializer = MemberSerializer(members,many=True)
+        return Response(serializer.data)
+
+    if request.method == "POST":
+        serializer = MemberSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET','PUT','DELETE'])
+def member_detail(request,pk):
+    """
+    Retrieve, update or delete a member.
+    """
+    try:
+         members = Member.objects.get(pk=pk)
+    except Member.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == "GET":
+        serializer = MemberSerializer(user)
+        return Response(serializer.data)
+    
+    if request.method == "PUT":
+        serializer = MemberSerializer(user,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    if request.method == "DELETE":
+        member.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
    
