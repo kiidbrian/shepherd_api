d410f35bff36776e1682932d430e09e0811ba038
Brian Paintsil <brnpaintsil@gmail.com>
2019-01-27 02:18:20 +0000

    clean ups

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,7 @@ from api import views
 
 urlpatterns = [
     path('users/', views.user_list),
-    path('users/<int:pk>', views.user_detail)
+    path('users/<int:pk>', views.user_detail),
     path('members/', views.member_list),
     path('members/<int:pk>', views.member_detail)
 ]
